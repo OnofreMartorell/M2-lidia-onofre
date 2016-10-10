3ecc79eb6a577bcca5df08824cadf92ec4d9703e
Lidia Talavera <lidia_tama@hotmail.com>
2016-10-10 12:29:55 +0200

    1

---
+++ Block1/Interpolation.m
@@ -0,0 +1,14 @@
+clear all
+I=imread('image1_toRestore.jpg');
+I=double(I);
+[m n]= size(I);
+[x,y] = meshgrid(1:n, 1:m); % rejilla imagen inicial
+r=1; % factor de escala
+[p,q]=meshgrid(1:r:n, 1:r:m); % rejilla imagen final
+I2=interp2(x,y,I,p,q,'nearest'); % interpolación
+% 'nearest','bilinear','bicubic'
+figure
+subplot(1,2,1),imagesc(I),axis image
+title('Original','FontSize',18)
+subplot(1,2,2),imagesc(I2),axis image
+title('Interpolador NN ','FontSize',18)
